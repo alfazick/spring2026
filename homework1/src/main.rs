af0ad8af2c3379d5ba4835ecccc6ad34f2bc90e6
Askar Nurbekov <nurbekoff@gmail.com>
2026-02-05 18:04:36 +0000

    h

--- homework1/src/main.rs
+++ homework1/src/main.rs
@@ -1,3 +1,17 @@
+
+fn ass1(){
+    println!("Run results");
+}
+fn ass2(){
+    println!("Run results");
+}
+fn ass3(){
+    println!("Run results");
+}
+
 fn main() {
-    println!("Hello, world!");
+    ass1();
+     ass2();
+      ass3();
+
 }
